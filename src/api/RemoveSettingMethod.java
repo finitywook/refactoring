7beb1464434764018dd2ee5b971879735a0bb826
유한욱 <ryu@hanwook.me>
2022-09-13 17:21:14 +0900

    Feat: Remove Setting Method(세터 제거하기) 추가

---
+++ src/api/RemoveSettingMethod.java
@@ -0,0 +1,48 @@
+package api;
+
+/**
+ * Remove Setting Method
+ *
+ * 세터 제거하기
+ */
+public class RemoveSettingMethod {
+
+    static class Old {
+        private String name;
+        private int age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }
+
+    static class New {
+        private final String name;
+        private final int age;
+
+        public New(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
+}
